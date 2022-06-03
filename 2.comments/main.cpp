ebfecde82d6ff17365fda2a4fcc7710bf4628270
Saurabh Chopra <Saurabh.Chopra.2021@live.rhul.ac.uk>
2022-06-03 13:32:04 +0100

    add comments cpp

---
+++ 2.comments/main.cpp
@@ -0,0 +1,16 @@
+// one line comment
+
+// this brings out the iostream library
+#include <iostream>
+
+/*
+    This is a block comment that englobes multiple
+    lines of text
+*/
+
+int main(){
+    // this uis going to print Hello World! to the console
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    //program ends here
+}
